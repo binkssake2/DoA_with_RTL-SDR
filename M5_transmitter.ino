2cbf3f920002d7e192e2873fbc305da5fadfb10f
felipereyel <felipe.reyel@gmail.com>
2020-10-10 12:19:31 -0300

    Update M5_transmitter.ino

--- M5_transmitter.ino
+++ M5_transmitter.ino
@@ -1,35 +1,45 @@
 #include <RH_ASK.h>
 #include <SPI.h> // Not actually used but needed to compile
 #include <M5StickC.h>
-#include <stdio.h>
-#include <stdlib.h>
 
 RH_ASK driver;
+char data[1];
 
-void setup()
-{
+void setup() {
   M5.begin();
-  M5.Lcd.setTextSize(3);
-    Serial.begin(9600);    // Debugging only
-    if (!driver.init())
-         Serial.println("init failed");
+  M5.Lcd.setTextSize(2);
+  if (!driver.init()) {
+    M5.Lcd.print("driver init failure");
+    while (1) {}
+  }
 }
 
-void loop()
-{   
-    char data[1];
-    
-    for (char i = 32; i < 128; i++)
-    {
-    
+void sendChars(int delayMilis) {
+  M5.Lcd.fillScreen(BLACK);
+  M5.Lcd.setCursor(0, 0);
+  M5.Lcd.print("Transmiting...");
+  
+  for (char i = 32; i < 128 && !M5.BtnA.read(); i++) {
     data[0] = i ;
     driver.send((uint8_t *)data, strlen(data));
     driver.waitPacketSent();
-    M5.Lcd.setCursor(0,0);
-    M5.Lcd.print(i);
-    delay(100);
-    M5.Lcd.fillScreen(BLACK);
-    }
-    
-    M5.update();    
+    // M5.Lcd.fillScreen(BLACK);
+    // M5.Lcd.setCursor(0, 0);
+    // M5.Lcd.print(i);
+    delay(delayMilis);
+  }
+}
+
+void loop() {
+  M5.Lcd.fillScreen(BLACK);
+  M5.Lcd.setCursor(0, 0);
+  M5.Lcd.print("Press A to begin");
+  
+  while(!M5.BtnA.read()){ delay(150); }
+  while(M5.BtnA.read()){ delay(150); }
+
+  while(!M5.BtnA.read()) { sendChars(10); }
+  while(M5.BtnA.read()){ delay(150); }
+  
+  M5.update();
 }
